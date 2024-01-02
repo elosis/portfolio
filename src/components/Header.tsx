b07b2b6cf0aae08e6423d41f7ea6f6ce21e1f0f4
elosis <96085941+elosis@users.noreply.github.com>
2024-01-03 00:32:50 +0300

    removing vercel dashboard link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,13 @@ const Header = () => {
   return (
     <div>
       <div className="nav-container">
-        <span className="first-page-icon double-icons">
+        <span className="first-page-icon">
           <a
             href="https://github.com/elosis?tab=repositories"
             style={{ color: "inherit", textDecoration: "none" }}
           >
             {" "}
             <AiFillGithub />
-          </a>
-          <a
-            href="https://vercel.com/elosis"
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
-            {" "}
-            <RxVercelLogo />
           </a>
         </span>
       </div>
